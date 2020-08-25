dddc68d8a2fc8e7979b3616fe797783add346f4d
nbrockett <n.brockett@gmail.com>
2020-08-25 15:57:06 +0100

    added swap chain

--- Hyades/src/Renderer/RenderContext.hpp
+++ Hyades/src/Renderer/RenderContext.hpp
@@ -48,6 +48,15 @@ namespace Hyades
         VkQueue m_graphics_queue;
         VkQueue m_present_queue;
 
+        // swap chain vars
+        VkSwapchainKHR swapChain;
+        std::vector<VkImage> swapChainImages;
+        VkFormat swapChainImageFormat;
+        VkExtent2D swapChainExtent;
+        std::vector<VkImageView> swapChainImageViews;
+        std::vector<VkFramebuffer> swapChainFramebuffers;
+
+
         const bool use_validation_layers = true;
 
         const std::vector<const char*> validationLayers = {
@@ -75,11 +84,18 @@ namespace Hyades
         void create_surface();
         void choose_physical_device();
         void create_logical_device();
+        void create_swap_chain();
 
         bool is_device_suitable(VkPhysicalDevice device);
         bool check_device_extension_support(VkPhysicalDevice device);
         QueueFamilyIndices find_queue_families(VkPhysicalDevice device);
         SwapChainSupportDetails query_swap_chain_support(VkPhysicalDevice device); 
+
+        VkSurfaceFormatKHR chooseSwapSurfaceFormat(const std::vector<VkSurfaceFormatKHR>& availableFormats);
+        VkPresentModeKHR chooseSwapPresentMode(const std::vector<VkPresentModeKHR>& availablePresentModes); 
+        VkExtent2D chooseSwapExtent(const VkSurfaceCapabilitiesKHR& capabilities);
+
+
 
         static void on_window_resize(uint32_t width, uint32_t height);
 
